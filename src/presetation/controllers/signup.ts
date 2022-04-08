da624ada5b26917ea182bccf8c5ebbe30ef281eb
Lucas Nonato <lucas.nonato1@hotmail.com>
2022-04-08 05:33:02 -0300

    feat: ensure SignUpController returns 400 if empty body is provided

--- src/presetation/controllers/signup.ts
+++ src/presetation/controllers/signup.ts
@@ -3,9 +3,11 @@ import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helpers'
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
+    const bodyLength = Object.keys(httpRequest.body).length
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+
     for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
+      if (!httpRequest.body[field] && bodyLength !== 0) {
         return badRequest(new MissingParamError(field))
       }
     }
