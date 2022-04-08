610630ae778ea61b038ca0b083d843f64c46ed26
Lucas Nonato <lucas.nonato1@hotmail.com>
2022-04-08 20:52:53 -0300

    refactor: move sut creation to a factory helper

--- src/presetation/controllers/signup.spec.ts
+++ src/presetation/controllers/signup.spec.ts
@@ -1,9 +1,13 @@
 // SUT = SYSTEM UNDER TEST
 import { SignUpController } from './signup'
 import { MissingParamError } from '../errors/missing-param-error'
+
+const makeSut = (): SignUpController => {
+  return new SignUpController()
+}
 describe('Signup Controller', () => {
   test('Should return 400 if no name is provided', () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         email: 'any_email@email.com',
@@ -17,7 +21,7 @@ describe('Signup Controller', () => {
   })
 
   test('Should return 400 if no email is provided', () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -31,7 +35,7 @@ describe('Signup Controller', () => {
   })
 
   test('Should return 400 if no password is provided', () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -45,7 +49,7 @@ describe('Signup Controller', () => {
   })
 
   test('Should return 400 if no passwordConfirmation is provided', () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -59,7 +63,7 @@ describe('Signup Controller', () => {
   })
 
   test('Should return 400 if empty body is provided', () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
     const httpRequest = {
       body: {}
     }
