119e94fa00157297913beaaf81389db7f3278cfe
Lucas Nonato <lucas.nonato1@hotmail.com>
2022-04-08 22:54:35 -0300

    feat: return 400 if an invalid emai is provided

--- src/presetation/controllers/signup.ts
+++ src/presetation/controllers/signup.ts
@@ -1,17 +1,33 @@
 import { HttpResponse, HttpRequest } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
-import { badRequest } from '../helpers/http-helpers'
+import { InvalidParamError } from '../errors/invalid-param-error'
+import { badRequest, goodRequest } from '../helpers/http-helpers'
 import { Controller } from '../protocols/controller'
+import { EmailValidator } from '../protocols/email-validator'
 export class SignUpController implements Controller {
+  private readonly emailValidator: EmailValidator
+
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   handle (httpRequest: HttpRequest): HttpResponse {
     const bodyLength = Object.keys(httpRequest.body).length
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
-
     for (const field of requiredFields) {
       if (!httpRequest.body[field] && bodyLength !== 0) {
         return badRequest(new MissingParamError(field))
       }
     }
-    return badRequest(new MissingParamError('no body'))
+
+    if (bodyLength === 0) {
+      return badRequest(new MissingParamError('no body'))
+    }
+    const emailteste = this.emailValidator.isValid(httpRequest.body.email)
+
+    if (!emailteste) {
+      return badRequest(new InvalidParamError('email'))
+    }
+    return goodRequest()
   }
 }
--- src/presetation/helpers/http-helpers.ts
+++ src/presetation/helpers/http-helpers.ts
@@ -4,3 +4,8 @@ export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: error
 })
+
+export const goodRequest = (): HttpResponse => ({
+  statusCode: 200,
+  body: 'Ok'
+})
